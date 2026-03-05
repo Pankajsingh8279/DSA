2bf7949f454197171c0248e6d075919ba6184f4c
Pankaj singh <pankajsingh827928@gmail.com>
2026-03-05 20:19:36 +0530

    Add files via upload

---
+++ 100 days code/Q33.c
@@ -0,0 +1,80 @@
+/*: Convert an infix expression to postfix notation using stack.
+
+Input:
+- Single line: infix expression (operands are single characters)
+
+Output:
+- Print the postfix expression
+
+Example:
+Input:
+A+B*C
+
+Output:
+ABC*+
+
+Explanation:
+Operator precedence: * > +
+Use stack to handle operator precedence and associativity*/
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+char stack[MAX];
+int top = -1;
+void push(char value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+char pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return '\0';
+    }
+    return stack[top--];
+}
+int precedence(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    }
+    if (op == '*' || op == '/') {
+        return 2;
+    }
+    return 0;
+}
+void infixToPostfix(char* infix, char* postfix) {
+    int j = 0;
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char token = infix[i];
+        if ((token >= 'A' && token <= 'Z') || (token >= 'a' && token <= 'z')) {
+            postfix[j++] = token;
+        } else if (token == '(') {
+            push(token);
+        } else if (token == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); 
+        } else { 
+            while (top != -1 && precedence(stack[top]) >= precedence(token)) {
+                postfix[j++] = pop();
+            }
+            push(token);
+        }
+    }
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+int main() {
+    char infix[MAX], postfix[MAX];
+    printf("Enter infix expression: ");
+    fgets(infix, MAX, stdin);
+    infix[strcspn(infix, "\n")] = '\0'; 
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+    return 0;
+}
