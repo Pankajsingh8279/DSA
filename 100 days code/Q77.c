280b44e3bc6d05bdde5aa2ee06782d92c9ec66c3
Pankaj singh <pankajsingh827928@gmail.com>
2026-04-18 22:05:43 +0530

    Add files via upload

---
+++ 100 days code/Q77.c
@@ -0,0 +1,65 @@
+/*Using BFS or DFS, check if the entire graph is connected.
+
+Input Format
+n m
+edges
+
+Output Format
+CONNECTED
+NOT CONNECTED
+
+Sample Input
+4 2
+1 2
+3 4
+
+Sample Output
+NOT CONNECTED*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int graph[100][100];
+int visited[100];
+int n, m;
+
+void dfs(int node)
+{
+    visited[node] = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(graph[node][i] == 1 && visited[i] == 0)
+        {
+            dfs(i);
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        graph[u][v] = 1;
+        graph[v][u] = 1;
+    }
+
+    dfs(1);
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(visited[i] == 0)
+        {
+            printf("NOT CONNECTED");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED");
+
+    return 0;
+}
